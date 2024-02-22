48ff6fd6db2152a264fb77919ba8fa9b36b2ffc3
michaelgutkind <mikegutkind@gmail.com>
2024-02-22 10:16:34 -0500

    configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 export default defineNuxtConfig({
-  baseURL: 'https://Mgutkind3.github.io/<repository>/', // Replace with your info
+  baseURL: 'https://Mgutkind3.github.io/finance-freak/', // Replace with your info
   build: {
     // Adjust other build options if needed
   }
