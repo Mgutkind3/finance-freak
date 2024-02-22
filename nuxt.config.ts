07cf72adbefefd1d6626c09e5642ce85054d39c7
michaelgutkind <mikegutkind@gmail.com>
2024-02-22 10:14:08 -0500

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+export default defineNuxtConfig({
+  baseURL: 'https://Mgutkind3.github.io/<repository>/', // Replace with your info
+  build: {
+    // Adjust other build options if needed
+  }
+})
